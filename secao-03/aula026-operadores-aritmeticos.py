6d86588a1b2898041dd3dfd951f849b538d6d569
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2025-08-08 21:52:22 -0300

    Curso de Python 3, 08/08/2025 (noite)

--- secao-03/aula026-operadores-aritmeticos.py
+++ secao-03/aula026-operadores-aritmeticos.py
@@ -1,3 +1,30 @@
+"""
+- Quando realizamos uma operação entre um número inteiro e um número com ponto flutuante, o resultado sempre será um valor com ponto flutuante.
+"""
+
+# Exponenciação:
+exponenciacao = 10 ** 3
+print("Exponenciação:", exponenciacao)
+
+# Multiplicação:
+multiplicacao = 10 * 10
+print('Multiplicação:', multiplicacao)
+
+# Divisão: sempre retornará um float.
+divisao = 10 / 3
+print('Divisão:', divisao)
+
+# Divisão inteira: sempre retornará um valor inteiro, mesmo com um tipo flutuante.
+divisao_inteira = 10 // 3
+print("Divisão inteira:", divisao_inteira)
+
+divisao_inteira = 10 // 2
+print("Divisão inteira:", divisao_inteira)
+
+# Módulo: imprime o resto da divisão.
+modulo = 55 % 2
+print("Módulo:", modulo)
+
 # Adição:
 adicao = 10 + 10
 print('Adição:', adicao)
@@ -5,23 +32,3 @@ print('Adição:', adicao)
 # Subtração:
 subtracao = 10 - 5
 print('Subtração:', subtracao)
-
-# Multiplicação:
-multiplilcacao = 10 * 10
-print('Multiplicação:', multiplilcacao)
-
-# Divisão: sempre retornará um float.
-divisao = 10 / 3
-print('Divisão:', divisao)
-
-# Divisão inteira: sempre retornará um valor inteiro, mesmo com o tipo flutuante.
-divisao_inteira = 10 // 3
-print('Divisão inteira', divisao_inteira)
-
-# Exponenciação:
-exponenciacao = 2 ** 10
-print('Exponenciação:', exponenciacao)
-
-# Módulo: imprime o resto da divisão
-modulo = 55 % 2
-print('Módulo:', modulo)
