239c6a61ef2d32bbd2aa7a138f4f7bbe7125ca2d
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2025-08-26 15:59:42 -0300

    Curso de Python 3, 26/08/2025 (tarde)

--- secao-03/aula040-operador-logico-and.py
+++ secao-03/aula040-operador-logico-and.py
@@ -1,26 +1,39 @@
-# Operadores lógicos:
-# and (e), or (ou), not (não).
+"""
+Operadores lógicos:
+and (e), or (ou), not (não).
  
-# and - Todas as condições precisam ser verdadeiras.
+and - Todas as condições precisam ser verdadeiras.
 
-# Se qualquer valor for considerado falso, a expressão inteira será avaliada naquele valor.
-# Considerados falsy (considerado falso): 0, 0.0, '', False.
-# Também existe o tipo 'None' que é usado para representar um 'não valor'.
+Se qualquer valor for considerado falso, a expressão inteira será avaliada naquele valor.
 
+Considerados falsy (considerado falso): 0, 0.0, ""(string vazia), False.
 
-entrada = input('[E]ntrar [S]air: ')
-senha_digitada = input('Senha: ')
+Também existe o tipo 'None' que é usado para representar um 'não valor'.
+"""
 
-senha_permitida = '123456'
+# Primeiro exemplo de uso do "and":
+entrada = input("[E]ntrar [S]air: ")
+senha_digitada = input("Senha: ")
+senha_permitida = "123456"
 
-if entrada == 'E' and senha_digitada == senha_permitida:
-    print('Entrar')
-
+if entrada == "E" and senha_digitada == senha_permitida:
+    print("Entrar")
 else:
-    print('Sair')
+    print("Sair")
 
+print("------------------")
 
-# Avaliação de curto circuito
-print('Avaliação de curto circuito')
+# Segundo exemplo de uso do "and": Avaliação de curto circuito.
+print(True and True)
 print(True and True and True)
 print(True and False and True)
+print("------------------")
+
+
+# Terceiro exemplo de uso do "and": Avaliação de curto circuito.
+print(bool("")) # Retorna False
+print(bool(" ")) # Retorna True
+print(bool(0)) # Retorna False
+print(bool(0.0)) # Retorna False
+print(True and 0 and True) # Retorna 0
+print("------------------")
--- secao-03/aula041-operador-logico-or.py
+++ secao-03/aula041-operador-logico-or.py
@@ -1,29 +1,41 @@
-# Operadores lógicos:
-# and (e), or (ou) e not (não).
+"""
+Operadores lógicos:
+and (e), or (ou) e not (não).
 
-# or (ou) - Qualquer condição verdadeira avalia toda a expressão como verdadeira.
+or (ou) - Qualquer condição verdadeira avalia toda a expressão como verdadeira.
 
-# Se qualquer valor for considerado verdadeiro, a expressão inteira será avaliada naquele valor.
-# São considerados falsy (considerado falso): 0, 0.0, '', False.
-# Também existe o tipo 'None' que é usado para repersentar um 'não valor'.
+Se qualquer valor for considerado verdadeiro, a expressão inteira será avaliada naquele valor.
 
+São considerados falsy (considerado falso): 0, 0.0, '', False.
 
-entrada = input('[E]ntrar [S]air: ')
-senha_permitida = '123456'
+Também existe o tipo 'None' que é usado para repersentar um 'não valor'.
+"""
 
-if (entrada == 'E' or entrada == 'e') and entrada != '':
-    senha_digitada = input('Senha: ')
+# Primeiro exemplo de uso do "and":
+entrada = input("[E]ntrar [S]air: ")
+senha_permitida = "123456"
+sair_str = "Sair"
+
+if entrada == "E" or entrada == "e":
+    senha_digitada = input("Senha: ")
 
     if senha_digitada == senha_permitida:
-        print('Entrar')
+        print("Entrou")
+    else:
+        print(f"{sair_str}")
 
 else:
-    print('Sair')
+     print(f"{sair_str}")
+print()
 
+# Segundo exemplo de uso do "and": Avaliação de curto circuito.
+print(True or False) # Retorna True
+print(True or False or 0) # Retorna True
+print(0 or False or 0 or 'abc') # Retorna "abc"
+print(0 or False or 0 or 'abc' or True) # Retorna "abc"
+print()
 
-# Avaliação de curto circuito
-print(True or False)
-print(0 or False or 0 or 'abc' or True)
-
-senha = input('Senha: ') or 'Sem senha'
+# Terceiro exemplo de uso do "and": Avaliação de curto circuito.
+senha = input("Senha: ") or "Sem senha"
 print(senha)
+print()
