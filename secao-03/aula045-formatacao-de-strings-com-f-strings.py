fc0ebcd0ff90ac4ceaff0ef712c2decee6ed09b1
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2025-08-29 15:22:00 -0300

    Curso de Python 3, 29/08/2025 (tarde)

--- secao-03/aula045-formatacao-de-strings-com-f-strings.py
+++ secao-03/aula045-formatacao-de-strings-com-f-strings.py
@@ -1,46 +1,60 @@
+"""
 # Formatação básica de strings:
-# s - string
-# d - int
-# f - float
-# .[número de dígitos]f
-# x [ou] X - Hexadecimal
-# [Caractere][><^][quantidade]
-#   > - Esquerda
-#   < - Direita
-#   ^ - Centro
-#   = - Força o número a aparecer antes dos zeros
-# Sinal - + [ou] -
-# Ex.: 0>-100,.1f
-# Conversion flags - !r [__repr__] !s [__str__] !a
 
+    s - string
+    d - int
+    f - float
+    .[número de dígitos]f
+    x ou X - Hexadecimal
+    
+    [Caractere][><^][quantidade]
+        
+        > - esquerda
+        < - direita
+        ^ - centro
+        = - força o sinal a aparecer antes dos zeros
+    
+    Sinal : + ou -
 
-variavel1 = 'ABC'
+        Ex.: 0>-100,.1f
+    
+    Conversion flags: !r [__repr__] !s [__str__] !a [__ask__]
+
+"""
+
+variavel1 = "ABC"
 variavel2 = 1000.4873648123746
 variavel3 = -1000.4873648123746
-variavel4 = 1500
+variavel_hexa = 1500
 
-print(f'{variavel1}.')
-print(f'{variavel1: >9}.')
-print(f'{variavel1: <9}.')
-print(f'{variavel1: ^9}.')
-print(f'{variavel1:*^9}.')
-print('\n')
+print(f"|{variavel1}|")
+print(f"|{variavel1: >9}|")
+print(f"|{variavel1: <9}|")
+print(f"|{variavel1: ^9}|")
+print(f"|{variavel1:*^9}|")
+print()
 
-print(f'{variavel2:.1f}')
-print(f'{variavel2:.2f}')
-print(f'{variavel2:,.3f}') # Usa o separador decimal vírgula [,]
-print(f'{variavel2:+,.4f}') # Exibe o valor com o [+] se for positivo
-print(f'{variavel3:-,.5f}') # Exibe o valor com o [-] se for negativo
-print(f'{variavel2:0=10,.3f}')
-print('\n')
+print(f"{variavel2}")
+print(f"{variavel2:.1f}") # Formata com uma casa decimal.
+print(f"{variavel2:.2f}") # Formata com duas casas decimais.
+print(f"{variavel2:,.3f}") # Formata com três casas decimais e com o separador decimal vírgula [,].
+print(f"{variavel2:+,.4f}") # Formata com quatro casas decimais e com o separador decimal vírgula. Se o número for positivo exibe o [+].
+print(f"{variavel3:-,.5f}") # Formata com cinco casas decimais e com o separador decimal vírgula. Se o número for negativo exibe o [-].
+print()
 
-print(f'{variavel4:x}')
-print(f'{variavel4:08x}')
-print(f'{variavel4:X}')
-print(f'{variavel4:08X}')
-print('\n')
+print(f"{variavel2:0>+10,.1f}")
+print(f"{variavel2:0=+10,.1f}") # O [=] força o sinal a aparecer antes dos zeros.
+print()
 
-print(f'{variavel1}')
-print(f'{variavel1!r}')
-print(f'{variavel1!s}')
-print(f'{variavel1!a}')
+print(f"{variavel_hexa}")
+print(f"O hexadecimal de {variavel_hexa} é {variavel_hexa:x}")
+print(f"{variavel_hexa:08x}")
+print(f"{variavel_hexa:X}") # Coloca as letras em caixa alta.
+print(f"{variavel_hexa:08X}") # Coloca as letras em caixa alta.
+print()
+
+print(f"{variavel1}")
+print(f"{variavel1!r}")
+print(f"{variavel1!s}")
+print(f"{variavel1!a}")
+print()
