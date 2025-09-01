48477a40c502e32782cfb17141236a8ecd9e19f2
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2025-09-01 16:10:06 -0300

    Curso de Python 3, 01/09/2025 (tarde)

--- secao-03/aula049-introducao-ao-try-e-except.py
+++ secao-03/aula049-introducao-ao-try-e-except.py
@@ -4,32 +4,35 @@
 try - tentar executar o código.
 except - ocorreu algum erro ao tentar executar o código.
 
-isdigit: verifica se o usuário digitou "apenas números"
+Obs.: O "input()" sempre retornará uma string.
+      Tudo em Python é um objeto.
+
+isdigit: verifica se o usuário digitou apenas "números".
 """
 
-
-numero_str = input('Dobrar o número: ')
-print('\n')
-
-# O "if" não trata o erro, apenas executa o código.
-if numero_str.isdigit():
-    numero_float = float(numero_str)
-    print(f'O dobro de {numero_str} é {numero_float * 2}')
-
-else:
-    print('Isso não é um número.')
+print(123)
+print(456)
+# int("a") - ValueError: invalid literal for int() with base 10: 'a'
+print()
 
 
-print('\n*\n')
+numero_str = input("Vou dobrar o número que você digitar: ")
+print()
 
+# O "if" não evita erros (exceções), apenas executa o código.
+# if numero_str.isdigit():
+#     numero_float = float(numero_str)
+#     print(f"O dobro de {numero_str} é {numero_float * 2}.")
+# else:
+#     print("Isso não é um número.")
+# print()
 
 # O "try/except" trata o erro.
 try:
     print(type(numero_str), numero_str)
     numero_float = float(numero_str)
     print(type(numero_float), numero_float)
-    
-    print(f'O dobro de {numero_str} é {numero_float * 2}')
-
+    print(f"\nO dobro de {numero_str} é {numero_float * 2}.")
 except:
-    print('Isso não é um número.')
+    print("Isso não é um número.")
+print()
