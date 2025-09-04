c5b2997d2ac9d087a4e2a08a58d63ce8efcb0bc7
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2025-09-04 13:36:23 -0300

    Curso de Python 3, 04/09/2025 (tarde)

--- secao-03/aula061-operadores-de-atribuicao-com-operadores-aritmeticos.py
+++ secao-03/aula061-operadores-de-atribuicao-com-operadores-aritmeticos.py
@@ -1,11 +1,55 @@
-# Operadores de atribuição com operadores aritméticos
-# = **= *= /= //= %= += -=
+"""
+- Operadores de atribuição com operadores aritméticos:
 
+    =     -> Atribuição simples.
+    **=   -> Potenciação e atribuição.
+    *=    -> Multiplicação e atribuição.
+    /=    -> Divisão e atribuição.
+    //=   -> Divisão inteira e atribuição.
+    %=    -> Módulo e atribuição.
+    +=    -> Adição e atribuição.
+    -=    -> Subtração e atribuição.
 
+"""
+
+# Atribuição simples:
 contador = 10
+print(contador)
 
-while contador < 1000:
-    contador *= 2
-    print(contador)
+# Potenciação e atribuição:
+contador **= 2
+print(contador)
 
-print("\nAcabou")
+# Multiplicação e atribuição:
+contador *= 2
+print(contador)
+
+# Divisão e atribuição:
+contador /= 2
+print(contador)
+
+# Divisão inteira e atribuição:
+contador //= 2
+print(contador)
+
+# Módulo e atribuição:
+contador %= 2
+print(contador)
+
+# Adição e atribuição:
+contador += 2
+print(contador)
+
+# Subtração e atribuição:
+contador -= 2
+print(contador)
+print()
+
+
+# Exemplo com o while:
+numero = 2
+
+while numero <= 1024:
+    print(numero)
+    numero *= 2
+print()
