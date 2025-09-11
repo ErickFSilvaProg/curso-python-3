4116bd4a89dfa6c8edefa435a8aaafbfeba770b5
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2025-09-11 16:31:14 -0300

    Curso de Python 3, 11/09/2025 (tarde)

--- secao-03/aula063-while-lacos-internos.py
+++ secao-03/aula063-while-lacos-internos.py
@@ -1,18 +1,25 @@
-# Repetições internas
+"""
+- Repetições internas:
+
+    While (enquanto)
+    
+        Executa uma ação enquanto uma condição for verdadeira.
+        Loop infinito -> Quando um código não tem fim.
+
+"""
 
 qtd_linhas = 5
 qtd_colunas = 5
 linha = 1
-coluna = 1
 
 while linha <= qtd_linhas:
-
+    coluna = 1
+    
     while coluna <= qtd_colunas:
-        print(f"{linha=}, {coluna=}")
+        print(f"L:{linha} | C:{coluna}")
         coluna += 1
     
-    coluna = 1
-    linha += 1
     print()
+    linha += 1
 
-print("Acabou...")
+print("Acabou")
--- secao-03/aula064-exercicio-guiado-com-while.py
+++ secao-03/aula064-exercicio-guiado-com-while.py
@@ -1,20 +1,21 @@
-# Iterando strings co while.
+# Iterando strings com while.
 
-cont1 = 0
-cont2 = 0
-nome = "Erick"
-novo_nome = ""
+#                 1         2
+#       01234567890123456789012
+nome = "Erick Ferreira da Silva"
 tamanho_nome = len(nome)
 
-while cont1 < tamanho_nome:
-    print(nome[cont1])
-    cont1 += 1
-
-
+print(nome)
+print(tamanho_nome)
 print()
-while cont2 < tamanho_nome:
-    letra = nome[cont2]
-    novo_nome += f"*{letra}"
-    cont2 += 1
 
-print(novo_nome)
+
+indice = 0
+novo_nome = ""
+
+while indice < tamanho_nome:
+    novo_nome += f"*{nome[indice]}"
+    indice += 1
+
+print(f"{novo_nome}*")
+print()
