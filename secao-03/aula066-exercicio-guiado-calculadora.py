8cd9ca6e6b1998d0a1f8a42ff88736cab9d48795
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2025-09-16 15:37:05 -0300

    Curso de Python 3, 16/09/2025 (tarde)

--- secao-03/aula066-exercicio-guiado-calculadora.py
+++ secao-03/aula066-exercicio-guiado-calculadora.py
@@ -3,38 +3,34 @@
 
     startswith("[string]"): começa com...
     endswith("[string]"): termina com...
+
 """
 
 
-# Variáveis:
-numero_1 = ...
-numero_2 = ...
-operador = ...
+# Variáveis globais:
 numeros_validos = ... # Flag para validar números digitados.
-texto_resultado = "\nResultado da operação:"
-operadores_permitidos = "+-/*"
+texto_resultado = "\nResultado da operação: "
+operadores_permitidos = "+-*/"
 
 
-# - Calculadora com while:
+# Calculadora com while:
 while True:
-
     # - Tratando possíveis erros da entrada dos dados:
     try:
         # - Entrada de dados:
         numero_1 = float(input("Digite um número: "))
         numero_2 = float(input("Digite outro número: "))
-        operador = input("Digite o operador (+-/*): ")
+        operador = input("Digite o operador (+-*/): ")
         numeros_validos = True
-
     except:
         numeros_validos = None
-    
+	
 
     # - Reinicia o programa caso retorne algum erro:
     if numeros_validos is None:
         print("(O número digitado é inválido.)\n")
         continue # Retornará ao início do programa em caso de erro.
-
+    
     if operador not in operadores_permitidos:
         print("(O operador digitado é inválido.)\n")
         continue # Retornará ao início do programa em caso de erro.
@@ -42,21 +38,17 @@ while True:
     if len(operador) > 1:
         print("(Digite apenas um operador.)\n")
         continue # Retornará ao início do programa em caso de erro.
-    
+
 
     # - Realizando as operações:
     if operador == "+":
         print(texto_resultado, numero_1 + numero_2)
-
     elif operador == "-":
         print(texto_resultado, numero_1 - numero_2)
-
-    elif operador == "/":
-        print(texto_resultado, numero_1 / numero_2)
-
     elif operador == "*":
         print(texto_resultado, numero_1 * numero_2)
-
+    elif operador == "/":
+        print(texto_resultado, numero_1 / numero_2)
     else:
         print("Nunca deveria chegar aqui.")
 
@@ -68,7 +60,6 @@ while True:
 
     if sair is True:
         break
-
     else:
         print()
 
