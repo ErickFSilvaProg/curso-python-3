a6285b2b1c450c0576991a332da4e3615a7a0ccb
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2026-01-14 15:51:37 -0300

    Curso de Python 3, 14/01/2026 (tarde)

--- secao-03/aula020-tipo-str.py
+++ secao-03/aula020-tipo-str.py
@@ -1,31 +1,39 @@
-# Tipos String: Textos.
-
 """
-Python: Linguagem de programação.
-Tipagem: Dinâmica / Forte.
-str -> string -> texto.
-Strings são textos que estão dentro de aspas.
+    - Tipo String: Textos.
 
+        str -> string -> texto
+        Strings são textos que estão dentro de aspas.
+
+    Python: Linguagem de programação.
+    Tipagens - Dinâmica: O Python sabe qual é o tipo da variável apenas pelo valor inserido.
+               Forte: O Python garante a segurança evitando coerção automática entre tipos incompatíveis.
+
+               Exemplo: print(1 + '1') TypeError: unsupported operand type(s) for +: 'int' and 'str'
+    
 """
 
-# Tipagem dinâmica: O python sabe o tipo do dado inserido.
+# Exemplo de tipagem dinâmica:
+print('- Exemplo de tipagem dinâmica:')
 print(type(1234), 1234)
-print(type("1234"), "1234")
+print(type('1234'), '1234')
 print()
 
 # Aspas simples:
+print('- Aspas simples:')
 print(type('Erick Ferreira'), 'Erick Ferreira')
 print()
 
 # Aspas duplas:
-print(type("Programador 'Python'"), "Programador 'Python'")
+print('- Aspas duplas:')
+print(type("Erick Ferreira"), "Erick Ferreira")
 print()
 
 # Caractere de 'escape': Barra invertida '\'.
-print(type("Dev. Python e \"Cientista de Dados\""), "Dev. Python e \"Cientista de Dados\"")
+print('Caractere de escape: Barra invertida -> \ ')
+print(type("Dev Python e \"Programador Web\""), "Dev Python e \"Programador Web\"")
 print()
 
-# 'r': Utilizado em expressões regulares.
-# Também exibe o caractere de escape na tela.
-print(type(r"Dev. Python e \"Cientista de Dados\""), r"Dev. Python e \"Cientista de Dados\"")
+# 'r': Utilizado em expressoes regulares. Também exibe o caractere de escape na tela.
+print("- 'r' -> Utilizado em expressões regulares.")
+print(type(r"Dev. Python e \"Programador Web\""), r"Dev. Python e \"Programador Web\"")
 print()
